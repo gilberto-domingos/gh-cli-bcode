63d47645c056e9cce7f38fd1fbece56d03731bcb
gilberto-domingos <domingoshot@hotmail.com>
2024-06-07 18:48:04 -0300

    finished

--- dist/gh-cli/server/main.server.mjs
+++ dist/gh-cli/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-RJ5AKSEX.mjs";import"./chunk-YYGIVN2M.mjs";import"./chunk-UX3XNLUI.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-KKWUFO4N.mjs";import"./chunk-DAJ5JP3Q.mjs";import"./chunk-Z2QMZVLY.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle, MatCardSubtitle} from "@angular/material/card";
 
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent,
+    MatCardActions,
+    MatCardSubtitle
+  ],
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.scss'
 })
