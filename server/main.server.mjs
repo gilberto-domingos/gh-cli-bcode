bf6f1fb4ca81f10598c623ec01c48a4525cd50a7
gilberto-domingos <domingoshot@hotmail.com>
2024-06-06 22:44:18 -0300

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-BAY64PPC.mjs";import"./chunk-BH5TQVSC.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-JSUOROZH.mjs";import"./chunk-BH5TQVSC.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
